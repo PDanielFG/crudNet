205f58f2757fc26447a5c6593d0e7fff266168ac
daniel <pedrodaniel.fernandezguerrero@alum.uca.es>
2026-05-28 20:17:50 +0200

    Agregar archivos de proyecto.

---
+++ crudNetNet/Ejemplo.cs
@@ -0,0 +1,21 @@
+﻿ using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace crudNetNet
+{
+    public class Ejemplo
+    {
+        [Key]   //Clave primaria de la bd
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]   //El valor se genera auto en la bd
+        [Column("Id")]  //nombre de la columna en la bd
+        public int Id { get; set; } //se puede leer y modificar
+
+        [Column("Nombre")]
+        [StringLength(100)]     //Como no es la clave primaria, se establece otras propiedades como la longitud
+        public string? Nombre { get; set; } //la variable puede ser nula
+
+        [Column("Valor")]
+        public double? Valor { get; set; }
+    }
+
+}
