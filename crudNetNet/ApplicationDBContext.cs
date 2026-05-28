82a5eb5047032cdebfbc32ecadc4cb75bf52020c
daniel <pedrodaniel.fernandezguerrero@alum.uca.es>
2026-05-28 20:43:55 +0200

    Configuración de ApplicacionDBContext para trabajar con ENtity. Puente entre app y bd

---
+++ crudNetNet/ApplicationDBContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+//Puente entre mi app y la bd
+namespace crudNetNet
+{
+    public class ApplicationDbContext : DbContext       // :EntityFramework 
+    {
+        //constructor para recibir la config(connectionString...) y pasarla a EntityFramework
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        //Configura la bd manualamente
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+
+        //Esta clase representa una tabla en la bd, como solo hay una tabla ejemplos pues solo una clase
+        public DbSet<Ejemplo> EJEMPLOS { get; set; }
+
+    }
+
+}
